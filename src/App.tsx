36941e3d49bf39399bb3b3d3884dae1370510e06
sk <jsckhan@outlook.com>
2023-04-10 17:12:24 -0400

    proto-type: add banner and project card to demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import ProjectCard from "@components/ProjectCard";
+import ProjectCard2 from "@components/ProjectCard2";
+import Banner from "@components/Banner";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
       <ProjectCard />
+      <ProjectCard2 />
       <div></div>
       <Footer />
     </div>
