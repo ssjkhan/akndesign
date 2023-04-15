db9ce944bd233b626c5a98300d9824c942917cd3
sk <jsckhan@outlook.com>
2023-04-15 15:14:46 -0400

    feat: animate smooth collapse transition

--- src/components/ClickHere.tsx
+++ src/components/ClickHere.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 
 export default function ClickHere() {
-  const [show, setShow] = useState(false);
   const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
-  const rightEye = useRef<HTMLDivElement>(null);
-  const leftEye = useRef<HTMLDivElement>(null);
+  const eyeDiv = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
+  const content = useRef<HTMLDivElement>(null);
 
+  // collapsible component
   function handleClick() {
-    setShow(!show);
+    container.current?.classList.toggle("h-0");
+    container.current?.classList.toggle("h-44");
+    content.current?.classList.toggle("translate-y-6");
   }
 
   function handleMouseMove(event: MouseEvent) {
@@ -36,26 +39,25 @@ export default function ClickHere() {
   }, []);
 
   return (
-    <div>
-      <div className="w-full bg-alexRed h-auto flex justify-center text-white">
+    <div className="bg-alexRed py-1">
+      <div className="w-full h-auto flex justify-center text-white overflow-hidden">
         <div onClick={handleClick}>
-          <div className="flex justify-center align-center bg-alexRed border-2 border-white rounded-full">
+          <div className="flex justify-center align-center bg-alexRed border-[1px] border-white rounded-full">
             <div className="bg-alexRed rounded-full mr-2 px-2">
               What's here?
             </div>
             <div className="flex justify-center items-center bg-white rounded-full px-2">
               <div
-                ref={leftEye}
+                ref={eyeDiv}
                 style={{
-                  transform: `rotate(${calcAngle(leftEye)}deg)`,
+                  transform: `rotate(${calcAngle(eyeDiv)}deg)`,
                 }}
                 className="w-4 h-4 border-2 border-alexRed rounded-full after:absolute after:w-2 after:h-2 after:bg-alexRed after:rounded-full after:content-[' ']"
               >
               </div>
               <div
-                ref={rightEye}
                 style={{
-                  transform: `rotate(${calcAngle(leftEye)}deg)`,
+                  transform: `rotate(${calcAngle(eyeDiv)}deg)`,
                 }}
                 className="w-4 h-4 border-2 border-alexRed rounded-full after:absolute after:w-2 after:h-2 after:bg-alexRed after:rounded-full after:content-[' ']"
               >
@@ -64,11 +66,26 @@ export default function ClickHere() {
           </div>
         </div>
       </div>
-      {show && (
-        <div className="bg-sky-500 text-white flex justify-center">
-          Nothing but our love for you (dancing video)
+      <div
+        ref={container}
+        className="h-0 transition-all ease-in-out duration-700"
+      >
+        <div
+          ref={content}
+          className="text-white flex flex-wrap justify-center over-flow-hidden transition-transform ease-in delay-300 duration-300"
+        >
+          <div className="basis-full flex justify-center text-2xl pt-4 font-bold">
+            Nothin' but our love for you
+          </div>
+          <div className="basis-full flex justify-center py-2">
+            You're curious. You like to discover. The little things matter. So
+            we found a video just for you.
+          </div>
+          <div className="border-[1px] border-white rounded-full px-4 text-white">
+            <a href="https://www.youtube.com/watch?v=bzLaL0SVwwI">Click here</a>
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
