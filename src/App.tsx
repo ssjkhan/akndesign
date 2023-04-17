f9613376f7130e4607a6a115d3b3d50c24f0254b
sk <jsckhan@outlook.com>
2023-04-17 15:49:07 -0400

    feat: responsive deisgn for body

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "@components/Header";
+import Header from "@components/header/Header";
 import Footer from "@components/Footer";
 import ProjectCard from "@components/ProjectCard";
 import ProjectCard2 from "@components/ProjectCard2";
@@ -7,19 +7,24 @@ import Banner from "@components/Banner";
 import PortfolioArr from "@components/PortfolioArray";
 import AwardsList from "@components/AwardsList";
 import ClickHere from "@components/ClickHere";
+import PacmanSidebar from "@components/pacman-sidebar/PacmanSideBar";
+import About from "@components/About";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <ClickHere />
       <Header />
-      <Banner />
-      <ProjectCard />
-      <ProjectCard2 />
-      <PortfolioArr />
-      <AwardsList />
-      <Footer />
-    </div>
+      <div className="App mx-4 sm:mx-16 lg:mx-64">
+        <Banner />
+        <About />
+        <ProjectCard />
+        <ProjectCard2 />
+        <PortfolioArr />
+        <AwardsList />
+        <Footer />
+      </div>
+    </>
   );
 }
 
