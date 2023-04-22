f2f677cbd650ee78012af0a439ee46821dcf0c0b
sk <jsckhan@outlook.com>
2023-04-22 18:47:03 -0400

    feat: update assets import alias

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@components": path.resolve(__dirname, "./src/components/"),
-      "@assets": path.resolve(__dirname, "./assets/"),
+      "@assets": path.resolve(__dirname, "./src/assets/"),
     },
   },
   server: {
