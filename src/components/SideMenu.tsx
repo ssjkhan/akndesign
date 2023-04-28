f2e393886cbe6c35c1ce4bd0747b3a52691687f6
sk <jsckhan@outlook.com>
2023-04-27 23:06:35 -0400

    feat: side menu styling

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Logo from "@components/Logo";
 
 type SideMenuProps = {
-  setShow: React.Dispatch<React.SetStateAction<boolean>>;
   menuRef: React.RefObject<HTMLDivElement>;
 };
 
-export default function SideMenu({ setShow, menuRef }: SideMenuProps) {
+export default function SideMenu({ menuRef }: SideMenuProps) {
   function handleClose() {
-    setShow(false);
     menuRef.current?.classList.toggle("translate-x-full");
   }
 
@@ -19,10 +16,9 @@ export default function SideMenu({ setShow, menuRef }: SideMenuProps) {
     >
       <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25">
       </div>
-      <nav className="relative flex flex-col py-6 px-6 w-full h-full bg-white border-r overflow-y-auto">
+      <nav className="relative flex flex-col py-6 px-6 w-full h-full bg-alexTeal border-r overflow-y-auto">
         <div className="flex items-center mb-12 sm:mb-16  pb-2 border-b-2 border-alexTeal">
-          <Logo classNames="text-alexRed w-16" />
-          <button className="navbar-close ml-auto" onClick={handleClose}>
+          <button className="navbar-close" onClick={handleClose}>
             <svg
               className="h-6 w-6 text-slate-900 cursor-pointer hover:text-gray-500"
               xmlns="http://www.w3.org/2000/svg"
@@ -39,12 +35,13 @@ export default function SideMenu({ setShow, menuRef }: SideMenuProps) {
               </path>
             </svg>
           </button>
+          <div className="text-alexRed text-2xl ml-auto">Menu</div>
         </div>
         <div className="text-alexRed">
           <ul>
             <li className="mb-1">
               <a
-                className="block p-4 text-sm font-semibold   hover:bg-gray-50 rounded"
+                className="block p-4 text-sm font-semibold hover:underline rounded"
                 href="#"
               >
                 Intro
@@ -52,7 +49,7 @@ export default function SideMenu({ setShow, menuRef }: SideMenuProps) {
             </li>
             <li className="mb-1">
               <a
-                className="block p-4 text-sm font-semibold   hover:bg-gray-50 rounded"
+                className="block p-4 text-sm font-semibold hover:underline rounded"
                 href="#"
               >
                 About
@@ -60,7 +57,7 @@ export default function SideMenu({ setShow, menuRef }: SideMenuProps) {
             </li>
             <li className="mb-1">
               <a
-                className="block p-4 text-sm font-semibold   hover:bg-gray-50 rounded"
+                className="block p-4 text-sm font-semibold hover:underline rounded"
                 href="#"
               >
                 Misfits
@@ -68,7 +65,7 @@ export default function SideMenu({ setShow, menuRef }: SideMenuProps) {
             </li>
             <li className="mb-1">
               <a
-                className="block p-4 text-sm font-semibold   hover:bg-gray-50 rounded"
+                className="block p-4 text-sm font-semibold hover:underline rounded"
                 href="#"
               >
                 Recent Work
@@ -76,7 +73,7 @@ export default function SideMenu({ setShow, menuRef }: SideMenuProps) {
             </li>
             <li className="mb-1">
               <a
-                className="block p-4 text-sm font-semibold   hover:bg-gray-50 rounded"
+                className="block p-4 text-sm font-semibold hover:underline rounded"
                 href="#"
               >
                 Awards & Press
@@ -87,19 +84,19 @@ export default function SideMenu({ setShow, menuRef }: SideMenuProps) {
         <div className="mt-auto">
           <div className="pt-6">
             <a
-              className="block px-6 py-2 mb-3 text-sm text-center text-gray-500 hover:text-gray-600 font-bold leading-loose border border-gray-100 hover:border-gray-200 rounded"
+              className="block px-6 py-2 mb-3 text-sm text-center text-alexRed hover:underline font-bold leading-loose rounded"
               href="#"
             >
               Connect with me on Linked In!
             </a>
             <a
-              className="block px-6 py-2 mb-2 text-sm text-center text-gray-500 hover:text-gray-600 font-bold leading-loose border border-gray-100 hover:border-gray-200 rounded"
+              className="block px-6 py-2 mb-2 text-sm text-center text-alexRed hover:underline font-bold leading-loose rounded"
               href="#"
             >
               Contact
             </a>
           </div>
-          <p className="mt-6 mb-4 text-sm text-center text-gray-400">
+          <p className="mt-6 mb-4 text-sm text-center text-alexRed">
             <span>© 2023 All rights aknDesigns</span>
           </p>
         </div>
