070af855fbaf77521c6402c0025a0d721596f062
sk <jsckhan@outlook.com>
2023-04-27 23:31:26 -0400

    feat: project definitions

---
+++ src/global.d.ts
@@ -0,0 +1,11 @@
+type ProjectInfo = {
+  // project title
+  title: string;
+  // project sub title
+  subTitle: string;
+  // project description
+  // each array entry is a new paragraph
+  description: string[];
+  // image paths in the src/assets/* folder
+  images: string[];
+};
