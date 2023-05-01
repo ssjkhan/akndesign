d06432eeca06c49a66756f1df7f8bb4a721cb3b4
sk <jsckhan@outlook.com>
2023-05-01 01:59:06 -0400

    feat: update theme custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ export default {
       colors: {
         "alexRed": "#FF4501",
         "alexTeal": "#7FFFD4",
+        "alexPink": "#D8BFD8",
+      },
+      fontFamily: {
+        "roobert": ["", "sans-serif"],
+        "reckless": ["", "sans-serif"],
+        "blogh": ["", "serif"],
+        "alpina": ["", "serif"],
+        "testing": ["Poppins", "sans-serif"],
       },
     },
   },
