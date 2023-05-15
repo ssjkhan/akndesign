a77cebe901930659a37b4fdeea3a053dca42e844
sk <jsckhan@outlook.com>
2023-05-15 16:29:21 -0400

    commit wip

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,18 @@
 export default function About() {
   return (
-    <div className="bg-alexRed text-white py-24 font-roobert">
-      <div className="mx-44 text-2xl">
+    <div className="bg-alexRed text-white py-24 font-roobert w-screen h-screen flex flex-wrap justify-center align-center">
+      <div className="mx-44 text-2xl basis-full lg:text-6xl m-b-0 pb-0 mb-0">
         My client-based work has received the attention of Buzzfeed, the Globe
         and Mail, and Wallpaper* Magazine.
       </div>
-      <div className="mx-64 text-base mt-8">
-        My freelance clients include Christophe Wylie( Cambridge Analytic),
+      <div className="mx-64  basis-full lg:text-6xl">
+        My freelance clients include Christophe Wylie (Cambridge Analytica),
         CityMapper, and UsTwo.
       </div>
-
-      <div className="text-base mx-auto px-8 w-44 border-2 border-white rounded-full flex justify-center mt-8">
-        Read More
+      <div className="text-base mx-auto px-8 flex justify-center mt-8 basis-full">
+        <div className="w-44 border-2 border-white rounded h-fit">
+          Read More
+        </div>
       </div>
     </div>
   );
